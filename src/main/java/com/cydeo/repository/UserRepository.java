82c94c38c4f712a6fb1b9b963511e0c652ec53ed
francis-d2020 <francis_d95@hotmail.>
2024-10-06 00:44:43 +0300

    in this part, I continued with creating the methods of theUserRepository. Also added new annotation(@transactional) in the repo

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUserName(String username);
+
+    @Transactional //so this method is intercepted by a code of try catch that makes
+    //sure that the deletion is successful in both UI and DB(commit) otherwise it rollback
+    void deleteByUserName(String username);
 }
